feb47520b5068b35630ef49b009d7cdc9a6753a6
Rodro-Kumar <rudrokormokar96@gmail.com>
2024-10-21 16:47:06 +0600

    modified by rodro

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Home from './Layout/Home'
+import React from "react";
+import Home from "./Layout/Home";
 import {
   createBrowserRouter,
   RouterProvider,
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
-import Routlayout from './Component/Routlayout';
+import Routlayout from "./Component/Routlayout";
+import About from "./Layout/About";
 
 function App() {
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route path='/' element={<Routlayout />}>
-          <Route path='/' element={<Home />} />
+        <Route path="/" element={<Routlayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Route>
       </Route>
     )
@@ -24,7 +25,7 @@ function App() {
     <div>
       <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Banner from '../Component/Banner'
-import Special_Dis from '../Component/Special_Dis'
+import React from "react";
+import Banner from "../Component/Banner";
+import Special_Dis from "../Component/Special_Dis";
+import Chef from "../Component/Chef";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner />
-            <Special_Dis />
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <Banner />
+      <Special_Dis />
+      <Chef />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
