84f65eca0dcaac3de604920c9cb2bd9a8e09aee8
mdabutalebdev <mdabutaleb.dev@gmail.com>
2024-10-21 16:33:49 +0600

    abutaleb

--- src/Component/Articels.jsx
+++ src/Component/Articels.jsx
@@ -18,7 +18,7 @@ const Articels = () => {
                         <p className={`text-[14px] text-prh2 font-montserrat font-medium leading-5 pt-6 pb-3 ml-3`}>January 3, 2023</p>
                         <h3 className={`text-2xl text-prh2 font-serif leading-6 font-medium ml-3`}>The secret tips & tricks to prepare a perfect burger & pizza for our customers</h3>
                         <p className='text-[14px] text-prh2 font-montserrat font-medium leading-5 pt-6 pb-3 ml-3'>Lorem ipsum dolor sit amet consectetur of a adipiscing elitilmim semper adipiscing massa gravida nisi cras enim quis nibholm varius amet gravida ut facilisis neque egestas.</p>
-                    </div>
+                    </div>                    
                     <div className="grid grid-cols-2 gap-10">
                         <BlogCard 
                           src={Blog_one}
